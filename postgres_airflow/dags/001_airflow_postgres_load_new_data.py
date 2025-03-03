bb6d8b3086a2c812bd276b905406cf1913e27848
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-03-03 19:02:46 +0100

    make automatic update of data

--- postgres_airflow/dags/001_airflow_postgres_load_new_data.py
+++ postgres_airflow/dags/001_airflow_postgres_load_new_data.py
@@ -30,34 +30,41 @@ def fetch_latest_record(table_name):
 
     with engine.connect() as conn:
         latest_record = conn.execute(sql_query).fetchone()
+        latest_record = latest_record[0] if latest_record else 1898786817
   
     return latest_record
 
 # function for fetching data from polygon API. Called by fetch_data_task task
 def fetch_data():
+    
     polygon_api_key = Variable.get("polygon_api_key")
     client = RESTClient(polygon_api_key)
 
     indices = Variable.get("tickers", deserialize_json=True)
     for indice in indices:
-    #     aggs = []
-    #     today = datetime.now()
-    #     start = today - timedelta(days=1500)
-    #     from_date = start.strftime('%Y-%m-%d')
-    #     to_date = today.strftime('%Y-%m-%d')
+        aggs = []
+        today = datetime.now()
+        start = today - timedelta(days=365)
+        from_date = start.strftime('%Y-%m-%d')
+        to_date = today.strftime('%Y-%m-%d')
 
-    #     for day in client.get_aggs(ticker=indice, multiplier=1, timespan='day', from_= from_date, to= to_date):
-    #         aggs.append(day)
+        for day in client.get_aggs(ticker=indice, multiplier=1, timespan='day', from_= from_date, to= to_date):
+            aggs.append(day)
 
-    #     df = pd.DataFrame(aggs)
+        df = pd.DataFrame(aggs)
 
         latest_record = fetch_latest_record(indice)
+
+        df_filtered = df[df['timestamp'] > latest_record]
+        engine = create_connection_to_postgres()
         print(latest_record)
+        print(df_filtered)
+        df_filtered.to_sql(indice, con=engine, if_exists='append', index=False)
 # DAG
 with DAG(
     dag_id = 'load_new_data',
     default_args = default_args,
-    schedule='0 19 * * *',
+    schedule='0 18 * * *',
     catchup = False,
     description = 'This DAG updates a table in PG and stores data from polygon API into it.',
     tags = ['database', 'postgres']
