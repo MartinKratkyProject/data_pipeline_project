b751ca8e46bbe2944a28207918580d6dc74c2fae
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-03-08 18:00:48 +0100

    add new api endpoints

--- flask_app_backend/route/routes.py
+++ flask_app_backend/route/routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from model.ticker_data.aapl_ticker_data import get_aapl_data
 from model.ticker_data.amzn_ticker_data import get_amzn_data
+from model.ticker_data.googl_ticker_data import get_googl_data
+from model.ticker_data.nvda_ticker_data import get_nvda_data
+from model.ticker_data.tsla_ticker_data import get_tsla_data
 
 routes_bp = Blueprint('routes', __name__)
 
@@ -15,3 +18,15 @@ def get_aapl_route():
 @routes_bp.route('/amzn', methods=['GET'])
 def get_amzn_route():
     return get_amzn_data()
+
+@routes_bp.route('/googl', methods=['GET'])
+def get_googl_route():
+    return get_googl_data()
+
+@routes_bp.route('/nvda', methods=['GET'])
+def get_nvda_route():
+    return get_nvda_data()
+
+@routes_bp.route('/tsla', methods=['GET'])
+def get_tsla_route():
+    return get_tsla_data()
