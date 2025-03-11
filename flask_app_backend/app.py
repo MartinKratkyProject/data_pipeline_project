987a14312bbfb112002075e2ab035629db473b17
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-03-11 18:37:08 +0100

    add CORS policy

--- flask_app_backend/app.py
+++ flask_app_backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from util.connection.db_connection import init_db
 from route.routes import routes_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://airflow:airflow@postgres:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
