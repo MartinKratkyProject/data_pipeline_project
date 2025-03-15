a1d65b536c0ca087f28e4a49dbb0a5edbc139cda
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-03-15 12:23:35 +0100

    add stock chart

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -11,7 +11,7 @@
         <RouterLink to="/page2">Page 2</RouterLink>
       </nav>
       </div>
-  </header>
+  </header> -->
 
   <RouterView />
 </template>
--- vue_frontend/src/views/Test1.vue
+++ vue_frontend/src/views/Test1.vue
@@ -4,7 +4,7 @@
     <p v-if="error">Error: {{ error }}</p>
     <p v-else-if="!open.length">Loading...</p>
     <p v-else>Latest Date: {{ data1?.[0]?.record_date }}</p>
-    <v-chart v-else :option="chartOption" style="width: 600px; height: 400px" />
+    <v-chart v-else :option="chartOption" class="chart" />
   </div>
 </template>
 
@@ -36,13 +36,31 @@ use([
 ]);
 
 const open = ref([]);
+const close = ref([]);
+const high = ref([]);
+const low = ref([]);
 const labels = ref([]);
 const data1 = ref([]);
 const error = ref(null);
 
 const chartOption = ref({
-  title: { text: 'Stock Opening Prices' },
+  title: { text: 'Apple Stock Prices' },
   tooltip: { trigger: 'axis' },
+  legend: {
+    show: true,
+    bottom: '5%', // Put legend at the bottom of the chart
+    left: 'center', // Center the legend
+    orient: 'horizontal', // Arrange items in a row
+    textStyle: { color: '#000', fontSize: 14 }, // Text style
+    backgroundColor: '#fff', // Optional, just for testing purposes
+    borderWidth: 1, // Optional, just for testing purposes
+  },
+  grid: {
+    top: '10%', // Keep some space on top for the title
+    left: '10%',
+    right: '10%',
+    bottom: '15%', // Adjust bottom to make space for the legend
+  },
   xAxis: {
     type: 'category',
     data: labels.value,
@@ -51,12 +69,10 @@ const chartOption = ref({
     type: 'value',
   },
   series: [
-    {
-      name: 'Open Price',
-      data: open.value,
-      type: 'line',
-      smooth: true,
-    },
+    { name: 'Open Price', data: open.value, type: 'line', smooth: true },
+    { name: 'Close Price', data: close.value, type: 'line', smooth: true },
+    { name: 'High Price', data: high.value, type: 'line', smooth: true },
+    { name: 'Low Price', data: low.value, type: 'line', smooth: true },
   ],
 });
 
@@ -73,10 +89,16 @@ const fetchData = async () => {
     data1.value.sort((a, b) => new Date(a.record_date) - new Date(b.record_date));
 
     open.value = [];
+    close.value = [];
+    high.value = [];
+    low.value = [];
     labels.value = [];
 
     for (let i = 0; i < data1.value.length; i++) {
       open.value.push(data1.value[i].open);
+      close.value.push(data1.value[i].close);
+      high.value.push(data1.value[i].high);
+      low.value.push(data1.value[i].low);
       labels.value.push(data1.value[i].record_date);
     }
     
@@ -85,13 +107,49 @@ const fetchData = async () => {
   }
 };
 
-watch(open, () => {
+watch([open, close, high, low], () => {
   chartOption.value = {
-    ...chartOption.value,
+    title: { text: 'Apple Stock Prices' },
+    tooltip: { trigger: 'axis' },
     xAxis: { type: 'category', data: labels.value },
-    series: [{ name: 'Open Price', data: open.value, type: 'line', smooth: true }],
+    yAxis: { type: 'value' },
+    series: [
+      {
+        name: 'Open Price',
+        data: open.value,
+        type: 'line',
+        smooth: true,
+      },
+      {
+        name: 'Close Price',
+        data: close.value,
+        type: 'line',
+        smooth: true,
+      },
+      {
+        name: 'High Price',
+        data: high.value,
+        type: 'line',
+        smooth: true,
+      },
+      {
+        name: 'Low Price',
+        data: low.value,
+        type: 'line',
+        smooth: true,
+      },
+    ],
   };
 });
 
+
 onMounted(fetchData);
 </script>
+<style>
+.chart {
+  width: 1000px;
+  height: 600px;
+  max-width: 100%;
+}
+
+</style>
