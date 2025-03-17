e62a7f7c98fe1c89f7dbd12f5a6c2b05e66b4c92
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-03-17 19:32:27 +0100

    update navbar

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -2,17 +2,25 @@
   <div>
     <nav>
       <ul>
-        <li><router-link to="/">Home</router-link></li>
+        <li>
+          <router-link to="/">
+            Home
+            <el-icon class="nav-icon"><HomeFilled /></el-icon>
+          </router-link>
+        </li>
         <li>
           <el-dropdown>
-            <span class="dropdown-trigger">Tickers</span>
+            <span class="dropdown-trigger">
+              Tickers
+              <el-icon class="nav-icon"><TrendCharts /></el-icon>
+            </span>
             <template #dropdown>
               <el-dropdown-menu>
-                <router-link to="/tickers/option1">Apple</router-link>
-                <router-link to="/tickers/option2">Amazon</router-link>
-                <router-link to="/tickers/option3">Google</router-link>
-                <router-link to="/tickers/option2">Nvidia</router-link>
-                <router-link to="/tickers/option3">Tesla</router-link>
+                <router-link class="dropdown-link" to="/tickers/option1">Apple</router-link>
+                <router-link class="dropdown-link" to="/tickers/option2">Amazon</router-link>
+                <router-link class="dropdown-link" to="/tickers/option3">Google</router-link>
+                <router-link class="dropdown-link" to="/tickers/option2">Nvidia</router-link>
+                <router-link class="dropdown-link" to="/tickers/option3">Tesla</router-link>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
@@ -22,9 +30,9 @@
             <span class="dropdown-trigger">Powered by</span>
             <template #dropdown>
               <el-dropdown-menu>
-                <router-link to="/tickers/option1">Vue3</router-link>
-                <router-link to="/tickers/option2">Element Plus</router-link>
-                <router-link to="/tickers/option3">Echarts</router-link>
+                <router-link class="dropdown-link" to="/tickers/option1">Vue3</router-link>
+                <router-link class="dropdown-link" to="/tickers/option2">Element Plus</router-link>
+                <router-link class="dropdown-link" to="/tickers/option3">Echarts</router-link>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
@@ -38,6 +46,7 @@
 </template>
 
 <script setup lang="ts">
+import { HomeFilled, TrendCharts } from "@element-plus/icons-vue";
 </script>
 
 <style scoped>
@@ -57,53 +66,42 @@ ul {
   margin: 0;
 }
 
-li {
-  font-size: 18px;
+li, .dropdown-trigger {
+  font-size: 20px;
 }
 
-nav a {
+nav a, .dropdown-trigger, .dropdown-link {
   color: white;
   font-weight: bold;
   padding: 10px 16px;
   border-radius: 8px;
   transition: background 0.2s ease-in-out, transform 0.2s;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  cursor: pointer;
 }
 
 nav a:hover, 
 .dropdown-trigger:hover,
-.el-dropdown-menu a:hover {
+.dropdown-link:hover {
   background: rgba(255, 255, 255, 0.2);
   transform: scale(1.05);
 }
 
-nav a, 
-.dropdown-trigger,
-.el-dropdown-menu a {
-  text-decoration: none;
-  color: white;
-  font-weight: bold;
-  padding: 10px 16px;
-  border-radius: 8px;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  transition: background 0.2s ease-in-out, transform 0.2s;
+.nav-icon {
+  margin-left: 5px;
 }
 
 .el-dropdown-menu {
   background-color: rgb(111, 118, 218);
-  color: black;
   display: flex;
   flex-direction: column;
   padding: 5px;
 }
 
-.el-dropdown-menu .el-dropdown-item, 
-.el-dropdown-menu a {
-  display: block;
-  padding: 8px 12px;
-  text-align: left;
-  color: white;
+.dropdown-link {
+  padding: 10px 16px;
 }
 
 </style>
